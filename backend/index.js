9d7aade7f90c77af9a4f560661bbc2b9efde3aff
Fernanda <femartinsoliveira@hotmail.com>
2020-01-16 01:39:18 -0300

    add mongoose && conectando ao atlas (mongodb)

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+mongoose.connect("mongodb+srv://omnistack:<omnistack>@cluster0-xjc9z.mongodb.net/week10?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.use(express.json());
 
